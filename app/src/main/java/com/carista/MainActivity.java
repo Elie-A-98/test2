0be08689ca9c6f4f5327fcd95f5d8066a3b4d2d2
Hussein SROUR <hussein.srour@intalio.com>
2021-02-03 08:07:42 +0200

    merge

--- app/src/main/java/com/carista/MainActivity.java
+++ app/src/main/java/com/carista/MainActivity.java
@@ -4,23 +4,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.viewpager.widget.ViewPager;
 
-import com.carista.data.db.AppDatabase;
 import com.carista.photoeditor.photoeditor.EditImageActivity;
 import com.carista.ui.main.SectionsPagerAdapter;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent=getIntent();
         setContentView(R.layout.activity_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
@@ -45,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         tabs.setSelectedTabIndicatorColor(Color.parseColor("#FF0000"));
         tabs.setTabTextColors(Color.parseColor("#727272"), Color.parseColor("#ffffff"));
 
-        if (getIntent() != null && getIntent().getAction()!= null && getIntent().getAction().equals("com.carista.MainActivity.UserFragment"))
+        if (getIntent() != null && getIntent().getAction() != null && getIntent().getAction().equals("com.carista.MainActivity.UserFragment"))
             viewPager.setCurrentItem(2);
     }
 
