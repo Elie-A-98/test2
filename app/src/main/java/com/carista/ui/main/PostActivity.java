96938bb48ad081c2d7b7fed851a8001f49e0d9d9
Hussein SROUR <hussein.srour@intalio.com>
2021-02-10 08:56:04 +0200

    post design

--- app/src/main/java/com/carista/ui/main/PostActivity.java
+++ app/src/main/java/com/carista/ui/main/PostActivity.java
@@ -2,31 +2,22 @@ package com.carista.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 import com.carista.R;
 import com.carista.SplashScreen;
 import com.carista.utils.FirestoreData;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -52,7 +43,7 @@ public class PostActivity extends AppCompatActivity {
         }
 
         postImage = findViewById(R.id.view_post_image);
-        likeCheckbox= findViewById(R.id.view_like_checkbox);
+        likeCheckbox = findViewById(R.id.view_like_checkbox);
         commentCheckbox = findViewById(R.id.view_comment_checkbox);
         likesCounterText = findViewById(R.id.view_likes_counter);
         titleText = findViewById(R.id.view_post_title);
@@ -62,48 +53,43 @@ public class PostActivity extends AppCompatActivity {
 
         String postId = null;
 
-        if(getIntent().getData()!=null)
+        if (getIntent().getData() != null)
             postId = getIntent().getData().toString().split("/post/")[1];
-        else if(getIntent().getExtras().getString("postId")!=null || !getIntent().getExtras().getString("postId").isEmpty()){
-            postId=getIntent().getExtras().getString("postId");
+        else if (getIntent().getExtras().getString("postId") != null || !getIntent().getExtras().getString("postId").isEmpty()) {
+            postId = getIntent().getExtras().getString("postId");
         }
 
         String finalPostId = postId;
         FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
-        firestore.collection("posts").whereEqualTo("id",postId).get().addOnCompleteListener(task -> {
-            if(task.isSuccessful()){
-                for(QueryDocumentSnapshot documentSnapshot: task.getResult()){
+        firestore.collection("posts").whereEqualTo("id", postId).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
                     String image = (String) documentSnapshot.get("image");
                     Picasso.get().load(image).resize(cardView.getWidth(), 600).centerCrop().into(postImage);
 
                     FirestoreData.getLikesCount(finalPostId, likesCounterText);
-                    FirestoreData.isLikedByUser(finalPostId,likeCheckbox,likesCounterText);
+                    FirestoreData.isLikedByUser(finalPostId, likeCheckbox, likesCounterText);
 
-                    String userId= (String) documentSnapshot.get("userId");
-                    String title= (String) documentSnapshot.get("title");
-                    FirestoreData.setPostNicknameTitle(userId, title, titleText);
+                    String userId = (String) documentSnapshot.get("userId");
+                    String title = (String) documentSnapshot.get("title");
+                    titleText.setText(title);
                     FirestoreData.setPostUserIconUsername(userId, userIcon, usernameText);
                 }
             }
         });
 
-        postImage.setOnClickListener(view -> {
-            return;
-        });
-
         likeCheckbox.setOnClickListener(view -> {
-            if(likeCheckbox.isChecked()){
+            if (likeCheckbox.isChecked()) {
                 FirestoreData.addLike(finalPostId);
-            }
-            else{
+            } else {
                 FirestoreData.removeLike(finalPostId);
             }
         });
 
         commentCheckbox.setOnClickListener(view -> {
-            Intent intent = new Intent(PostActivity.this,CommentsActivity.class);
-            intent.putExtra("postId",finalPostId);
+            Intent intent = new Intent(PostActivity.this, CommentsActivity.class);
+            intent.putExtra("postId", finalPostId);
             startActivity(intent);
         });
 
--- app/src/main/java/com/carista/utils/FirestoreData.java
+++ app/src/main/java/com/carista/utils/FirestoreData.java
@@ -153,7 +153,7 @@ public class FirestoreData {
         });
     }
 
-    public static void setPostUserIconUsername(String userId, CircleImageView userAvatar, TextView userNicknameText){
+    public static void setPostUserIconUsername(String userId, CircleImageView userAvatar, TextView userNicknameText) {
         FirebaseFirestore firestore = FirebaseFirestore.getInstance();
         firestore.collection("users").whereEqualTo("id", userId).addSnapshotListener((value, error) -> {
             for (DocumentSnapshot documentSnapshot : value.getDocuments()) {
@@ -162,16 +162,6 @@ public class FirestoreData {
                 if (!avatar.isEmpty())
                     Picasso.get().load(avatar).into(userAvatar);
                 userNicknameText.setText(nickname);
-            }
-        });
-    }
-
-    public static void setPostNicknameTitle(String user, String title, TextView userNicknameTitle) {
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        firestore.collection("users").whereEqualTo("id", user).addSnapshotListener((value, error) -> {
-            for (DocumentSnapshot documentSnapshot : value.getDocuments()) {
-                String nickname = (String) documentSnapshot.get("nickname");
-                userNicknameTitle.setText(Html.fromHtml("<b>" + nickname + "</b> " + title));
             }
         });
     }
